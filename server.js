9c19e34218a1ecb9648d8e73ff7dc7b1c0b25d89
Xandi <santosal2018@gmail.com>
2023-08-08 22:44:49 -0400

    fix error on server file and .env file

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = 3001;
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'secret',
@@ -26,11 +23,7 @@ const sess = {
   })
 };
 
-app.use(session(sess));
-
-// Initiate handlebars
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.use(session(sess))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
