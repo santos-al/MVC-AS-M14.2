ae2e6f78d8211ee5f23b7d5a7dd4d2ada2ab00a9
Xandi <santosal2018@gmail.com>
2023-08-08 22:05:45 -0400

    add connection.js

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+const path = require('path');
+
+// Add dotenv functionality to hide sensitive info
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASS, 
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+
+module.exports = sequelize;
