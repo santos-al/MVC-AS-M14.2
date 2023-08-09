c6cb3e498cbacafffe73feecc89612cd441cec0d
Xandi <santosal2018@gmail.com>
2023-08-08 22:14:25 -0400

    add a schema

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'secret',
   cookie: {
     maxAge: 300000,
     httpOnly: true,
